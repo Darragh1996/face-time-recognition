d59a20976b740c2769a08f72fa3d98a864ddd51e
Darragh Ferry <darraghferry@hotmail.com>
2020-08-13 20:36:53 +0100

    updated dependencies

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// import "@tensorflow/tfjs-node";
+import * as canvas from "canvas";
+import * as faceapi from "face-api.js";
+
 const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models")
+]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
@@ -8,4 +19,6 @@ function startVideo() {
   );
 }
 
-startVideo();
+video.addEventListener("play", () => {
+  console.log("success");
+});
